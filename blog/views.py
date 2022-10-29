812f5cfd1372daa7939165e24e2e0b0ef230204f
Андрей Кустов <andrey_kustov@192.168.1.5>
2022-10-30 00:07:26 +0300

    order by desc

--- blog/views.py
+++ blog/views.py
@@ -29,7 +29,7 @@ def serialize_tag(tag):
 
 
 def index(request):
-    most_popular_posts = Post.objects.annotate(count=Count('likes')).order_by('count')[:5]
+    most_popular_posts = Post.objects.annotate(count=Count('likes')).order_by('-count')[:5]
     fresh_posts = Post.objects.order_by('published_at')
     most_fresh_posts = list(fresh_posts)[-5:]
 
